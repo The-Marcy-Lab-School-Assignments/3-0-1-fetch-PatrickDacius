d9f7bfb50379861774f2eff8a580b9d4d1907ae1
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-06-12 23:58:12 -0400

    not fin

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,13 +1,45 @@
-const userUrl = 'https://jsonplaceholder.typicode.com/users'
+import { FetchError } from "node-fetch";
+
+let userUrl = 'https://jsonplaceholder.typicode.com/users'
 
 export const checkResponseStatus = () => {
+    return fetch(userUrl)
+        .then(response => {
+            return {
+                status: response.status,
+                ok: response.ok,
+                url: response.url
+            }
+        })
 };
+
+console.log(checkResponseStatus());
 
 export const getUsers = () => {
+    return fetch(userUrl)
+        .then(response => response.json())
+
 };
 
-export const getUserPosts = () => {
+export const getUserPosts = (userId) => {
+    userUrl = `https://jsonplaceholder.typicode.com/users/${userId}/posts`
+    return fetch(userUrl)
+        .then(response => response.json())
+        .then(post => post)
 };
 
-export const createNewUser = () => {
+export const createNewUser = (newUserData) => {
+    // let newUserData = { username: '', email: '' }
+    const postOption = {
+        method: "POST",                      // The type of HTTP request
+        body: JSON.stringify(newUserData),       // The data to be sent to the API
+        headers: {
+            "Content-Type": "application/json" // The format of the body's data
+        }
+    }
+
+    userUrl = 'https://jsonplaceholder.typicode.com/users'
+    return fetch(userUrl, postOption)
+        .then(response => response.json())
 }
+
--- src/render-functions.js
+++ src/render-functions.js
@@ -1,5 +1,5 @@
 export const setupPageBasics = (parentEl) => {
-    parentEl.innerHTML = `
+  parentEl.innerHTML = `
       <h1>Intro To Fetch!</h1>
       <div id='status'></div>
       <div id='users'>
@@ -21,23 +21,68 @@ export const setupPageBasics = (parentEl) => {
       <div id='new-user'></div>
     `;
 
-    const statusDiv = parentEl.querySelector('#status');
-    const usersUl = parentEl.querySelector('#users-list');
-    const postsUl = parentEl.querySelector('#posts-list');
-    const newUserForm = parentEl.querySelector('#new-user-form');
-    const newUserDiv = parentEl.querySelector('#new-user');
+  const statusDiv = parentEl.querySelector('#status');
+  const usersUl = parentEl.querySelector('#users-list');
+  const postsUl = parentEl.querySelector('#posts-list');
+  const newUserForm = parentEl.querySelector('#new-user-form');
+  const newUserDiv = parentEl.querySelector('#new-user');
 
-    return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
+  return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
 };
 
-export const renderStatus = () => {
+export const renderStatus = (statusDiv, statusInfoObj) => {
+  const statusHeading = document.createElement('h2')
+  statusHeading.textContent = `Info on - ${statusInfoObj.url}`
+  statusHeading.id = 'status-heading'
+
+  const statusPtag = document.createElement('p')
+  statusPtag.id = 'status-code'
+
+  let statusMessage = '';
+  if (statusInfoObj.ok) {
+    statusMessage = `Status code: ${statusInfoObj.status}, OK!`
+  } else {
+    statusMessage = `Status code: ${statusInfoObj.status}, FAIL!`
+  }
+
+  statusPtag.textContent = statusMessage
+  statusDiv.append(statusHeading, statusPtag)
 }
 
-export const renderUsers = () => {
+
+export const renderUsers = (usersUI, users) => {
+  usersUI.replaceChildren()
+  users.forEach(user => {
+    const userLi = document.createElement('li')
+    userLi.className = "user-card"
+    userLi.textcontent = `Username: ${username.name}, id: ${user.id}`
+
+    const button = document.createElement('button')
+    button.id = 'data-user-id'
+    button.textContent = `Load ${username.name}'s posts`
+
+    users.append(userLi)
+    userLi.append(button)
+
+  });
+
+
 };
 
 export const renderPosts = () => {
 }
 
-export const renderNewUser = () => {
+export const renderNewUser = (newUserDiv, newUserInfo) => {
+
+  newUserDiv.replaceChildren()
+  // newUserDiv.innerHTML = ''
+
+  const usernameH2 = document.createElement('h2');
+  usernameH2.textContent = newUserInfo.username
+
+  const emailProp = document.createElement('p');
+  emailProp.textContent = newUserInfo.email
+
+  newUserDiv.append(usernameH2)
+  newUserDiv.append(emailProp)
 }
